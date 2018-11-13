966ce6fc0557000c7cf87b0fd2eeb606eae5e3e0
Ramesh Ravi <ramesh7128@gmail.com>
2018-11-13 15:15:26 +0530

    edited fizz buzz using ternary operator

--- js/day1/project2/main.js
+++ js/day1/project2/main.js
@@ -56,17 +56,14 @@ function formatAmount(amount) {
 console.log(formatAmount(amount))
 
 // Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
+
 function fizzbuzz () {
     for(var i = 1; i<100; i++) {
-        if (i%3 === 0 && i%5 === 0) {
-            console.log("FizzBuzz");
-        } else if (i%3 === 0) {
-            console.log("Buzz");
-        } else if (i%5 === 0) {
-            console.log("Fizz");
-        } else {
-            console.log(i);
-        }
+        var message = (i%3 === 0 && i%5 === 0)? "FizzBuzz":
+            (i%3 === 0) ? "Fizz":
+            (i%5 === 0) ? "Buzz":
+            i;
+        console.log(message);
     }
 }
 
