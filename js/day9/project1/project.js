6a358200396e2f196628d53455c326e972a6961b
Ramesh Ravi <ramesh7128@gmail.com>
2018-11-28 17:39:10 +0530

    todo fixes

--- js/day9/project1/project.js
+++ js/day9/project1/project.js
@@ -1,34 +1,89 @@
 "use strict";
 // capture the input and append it to a list, later use localstorage.
-let todo = [];
-let deletedList = [];
-let doneList = [];
+let todo = {};
+let deletedList = {};
+let doneList = {};
+let count = 0;
+
+function randomGenerator() {
+    return Math.floor(Math.random() * Math.floor(25893));
+}
+
 document.addEventListener('keydown', function(event) {
     if (event.keyCode == '13') {
-        let value = document.getElementById("input-text").value;
-        let item = {
-            name: value,
-            done: false,
+        if (event.target.id=='input-text'){
+            let value = document.getElementById("input-text").value;
+            let key = randomGenerator();
+            if (!(/^ *$/.test(value))) {
+                let item = {
+                    id: key,
+                    name: value,
+                }
+                todo[key] = item;
+                document.getElementById("input-text").value = "";
+                display("todo-list-section");
+            }
+        } else if (event.target.id=='edit-text') {
+            let value = document.getElementById("edit-text").value;
+            let key =  document.getElementById("edit-text").dataset.key;
+            if (!(/^ *$/.test(value))) {
+                let item = {
+                    id: key,
+                    name: value,
+                }
+                todo[key] = item;
+                display("todo-list-section");
+            }
         }
-        todo.push(item);
-        var newElement = document.createElement('li');
-        newElement.innerHTML = `<span>${value}</span><span><button class='delete'>Delete</button><button class='done'>Done</button></span>`;
-        document.getElementById("todo-list-section").appendChild(newElement);
-        document.getElementById("input-text").value = "";
     }
 });
 
+function display(ulId) {
+    let iterArray = ((ulId == "todo-list-section")? todo: (ulId == "deleted-list-section") ? deletedList: doneList);
+    document.getElementById(ulId).innerHTML = '';
+    for (let key in iterArray) {
+        var newElement = document.createElement('li');
+        newElement.className = 'new-list-element';
+        newElement.innerHTML = '<div>';
+        newElement.innerHTML += `<span data-key=${iterArray[key].id} class='todo-element'>${iterArray[key].name}</span>`;
+        if (ulId == "todo-list-section") {
+            newElement.innerHTML += `<span class='action-icons'><i data-action="done" data-key=${iterArray[key].id} class="fa fa-check" aria-hidden="true"></i><i data-action="delete" data-key=${iterArray[key].id} class="fa fa-trash" aria-hidden="true"></i></span>`;
+        }
+        newElement.innerHTML += '</div>';
+        document.getElementById(ulId).appendChild(newElement);
+    }
+}
+
 document.addEventListener('click', function(event) {
-    let deleteToDo = document.getElementsByClassName('delete');
-    let doneToDo = document.getElementsByClassName('done');
-    if (event.target.tagName == "BUTTON") {
-        event.target.parentElement.parentElement.parentElement.removeChild(event.target.parentElement.parentElement);
+    if (event.target.tagName == "I") {
+        let action = event.target.dataset.action;
+        let id = event.target.dataset.key
+        if(action == 'delete') {
+            deletedList[id] = todo[id];
+            delete todo[id];
+            display("todo-list-section");
+            display('deleted-list-section');
+        } else if(action == "done") {
+            doneList[id] = todo[id];
+            delete todo[id];
+            display("todo-list-section");
+            display('done-list-section');
+        }
     }
-    if (event.target.className == 'delete') {
-        document.getElementById('deleted-list-section').appendChild(event.target.parentElement.parentElement);
-    } else if (event.target.className == 'done') {
-        document.getElementById('done-list-section').appendChild(event.target.parentElement.parentElement);
+});
+
+document.addEventListener('dblclick', function(event) {
+    let Oldevent = event;
+    display('todo-list-section');
+    if (event.target.tagName == 'SPAN') {
+        let id = event.target.dataset.key;
+        let inputElement = document.createElement('input');
+        inputElement.id = 'edit-text';
+        inputElement.dataset.key = id;
+        inputElement.value = todo[id].name;
+        let newSpanElement = document.querySelectorAll(`[data-key="${id}"]`)[0];
+        console.log(newSpanElement.parentElement.parentElement);
+        newSpanElement.parentElement.parentElement.replaceChild(inputElement, newSpanElement.parentElement);
     }
-    console.log("click registerd", deleteToDo);
 });
 
